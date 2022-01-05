702c8ec99f4fce7223f13052f608263e66fb9875
Saurabh Chopra <Saurabh.Chopra.2021@live.rhul.ac.uk>
2022-01-05 01:01:27 +0000

    new tests added

--- tests/test_discretization.py
+++ tests/test_discretization.py
@@ -2,4 +2,8 @@ from ..discretization import Discretization
 
 
 def test_discretization():
-    assert (Discretization().execute()) == 5.5
+    assert (Discretization(dataset_file_name="dataset.csv").execute()) == 10.0
+
+
+def test_discretization2():
+    assert (Discretization(dataset_file_name="dataset2.csv").execute()) == 5.5
